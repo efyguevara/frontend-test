1b114e475d6dea9e24bea78e8d244821786c92fa
Stefany Guevara <efyguevara@gmail.com>
2021-01-11 22:50:33 -0300

    Adding redux and actions and reducers files

--- src/index.js
+++ src/index.js
@@ -1,8 +1,24 @@
+import 'bootstrap/dist/css/bootstrap.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { applyMiddleware, createStore } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import thunk  from 'redux-thunk';
+import reducers from './reducers/index';
+
+const initialState = {
+  loading: false
+}
+
+const store = createStore(
+  reducers,
+  initialState,
+  composeWithDevTools(applyMiddleware(thunk)),
+);
 
 // You don't have to use `fetch` btw, use whatever you want
-const getCounters = () => 
+const getCounters = () =>
   fetch('/api/v1/counter', { method: 'get' })
     .then(res => res.json());
 
@@ -17,6 +33,10 @@ const App = () => {
 };
 
 ReactDOM.render(
-  <App />,
+<Provider store={store}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Provider>,
   document.getElementById('root')
 );
---
+++ src/reducers/index.js
@@ -0,0 +1,9 @@
+const reducers = (state, action) => {
+    switch (action.type) {
+      case 'ADD_TODO':
+        return state.concat([action.text])
+      default:
+        return state
+    }
+  }
+  export default reducers;
