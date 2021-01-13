ebf567eb96ac639559694dddaa81b90eae9a8ee2
Stefany Guevara <efyguevara@gmail.com>
2021-01-12 21:32:46 -0300

    Path of icon and called in the html modified

--- src/components/Searcher/Searcher.jsx
+++ src/components/Searcher/Searcher.jsx
@@ -1,17 +1,17 @@
 import React from 'react';
 import './index.css';
 import { FormControl, InputGroup } from 'react-bootstrap';
-import Glass from '../../assets/static/Glass.svg';
+import glass from '../../assets/static/glass.svg';
 
 
 const Searcher = () => {
-    
+    const iconSearch = <img src={glass} alt="glass icon" />;
     return (
         <>
             <div className="searcher">
                 <InputGroup>
                     <InputGroup.Prepend>
-                        <InputGroup.Text><img src={Glass} alt="glass icon" /></InputGroup.Text>
+                        <InputGroup.Text>{iconSearch}</InputGroup.Text>
                     </InputGroup.Prepend>
                     <FormControl className="searcher-without-border" id="inlineFormInputGroup" placeholder="Search counters" />
                 </InputGroup>
