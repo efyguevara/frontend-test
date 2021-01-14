c6ceab4eca39085af4a85b4fba56a2a39de0afb9
Stefany Guevara <efyguevara@gmail.com>
2021-01-14 14:17:51 -0300

    dispatch for getCounters()

--- src/templates/Welcome/Welcome.jsx
+++ src/templates/Welcome/Welcome.jsx
@@ -1,10 +1,22 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
+import { Link } from 'react-router-dom';
+import { getCountersService } from '../../services/counters';
+import { changeLoading } from '../../actions';
 import './index.css';
+import { Container } from 'react-bootstrap';
 import initialImage from '../../assets/static/initialImage.svg';
 import Btn from '../../components/Btn/Btn';
-import { Container } from 'react-bootstrap';
+
 
 const Welcome = () => {
+    const dispatch = useDispatch();
+
+    const fillCounters = () => {
+        dispatch(changeLoading(true))
+        dispatch(getCountersService())
+    }
+    
     return (
         <>
             <div className="welcome">
@@ -12,10 +24,9 @@ const Welcome = () => {
                     <img src={initialImage} className="initialImage d-block ml-auto mr-auto" alt="logo counter app" />
                     <h4 className="d-block text-center bold">Welcome to Counters</h4>
                     <p className="d-block text-center">Capture cups of lattes, frapuccinos, or anything else that can be counted.</p>
-                    <Btn title="Get Started" align="d-block ml-auto mr-auto" theme="main" size="welcome" />
+                    <Link to="/counters"> <Btn title="Get Started" align="d-block ml-auto mr-auto" theme="main" size="welcome" onClick={fillCounters} /></Link>
                 </Container>
             </div>
-
         </>
     )
 }
