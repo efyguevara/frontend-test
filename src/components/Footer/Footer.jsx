8989aa13dd01c4cf26178e638f6af981db652af9
Stefany Guevara <efyguevara@gmail.com>
2021-01-14 23:13:05 -0300

    Function to show modal for new counters

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,18 +1,24 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
 import './index.css';
 import { Container, Row, Col } from 'react-bootstrap';
 import Btn from '../Btn/Btn';
 import trash from '../../assets/static/trash.svg';
 import share from '../../assets/static/share.svg';
 import plus from '../../assets/static/plus.svg';
-
+import { createNewCounterModalAction } from '../../actions';
 
 const Footer = () => {
+    const dispatch = useDispatch();
 
     const trashIcon = <img src={trash} alt='plus icon' />;
     const shareIcon = <img src={share} alt='plus icon' />;
     const plusIcon = <img src={plus} alt='plus icon' />;
-
+    
+    const handleNewCounterModal = () => {
+        dispatch(createNewCounterModalAction(true))
+    }
+    
     return (
         <>
             <Container>
@@ -23,11 +29,12 @@ const Footer = () => {
                     </Col>
 
                     <Col xs={5} sm={4} md={6} lg={6} className="align-right">
-                        <Btn title={plusIcon} theme="main" size="footer" />
+                        <Btn title={plusIcon} theme="main" size="footer" onClick={handleNewCounterModal} />
                     </Col>
                 </Row>
             </Container>
         </>
     )
 }
+
 export default Footer;
