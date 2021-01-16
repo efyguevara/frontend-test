c5c9f0bdfa282f9385f2cbbb04b86f2ed34bdc50
Stefany Guevara <efyguevara@gmail.com>
2021-01-16 01:30:17 -0300

    Function 'handleDeleteCounter' to delete counters and functions 'handleShareCounter' and 'showShareComponent' to show the component of the share counters

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,31 +1,49 @@
 import React from 'react';
-import { useDispatch } from 'react-redux';
+import { connect, useDispatch } from 'react-redux';
 import './index.css';
-import { Container, Row, Col } from 'react-bootstrap';
+import { Container, Row, Col, OverlayTrigger } from 'react-bootstrap';
 import Btn from '../Btn/Btn';
 import trash from '../../assets/static/trash.svg';
 import share from '../../assets/static/share.svg';
 import plus from '../../assets/static/plus.svg';
-import { createNewCounterModalAction } from '../../actions';
+import { createNewCounterModalAction, shareCounter } from '../../actions';
+import ShareCounter from '../Counters/ShareCounter';
+import { deleteCounterService } from '../../services/counters';
 
-const Footer = () => {
+const Footer = ({ selectedCounterStore }) => {
     const dispatch = useDispatch();
 
-    const trashIcon = <img src={trash} alt='plus icon' />;
-    const shareIcon = <img src={share} alt='plus icon' />;
+    const trashIcon = <img src={trash} alt='trash icon' />;
+    const shareIcon = <img src={share} alt='share icon' />;
     const plusIcon = <img src={plus} alt='plus icon' />;
-    
+    const shareComponent = <ShareCounter />;
+
     const handleNewCounterModal = () => {
         dispatch(createNewCounterModalAction(true))
     }
-    
+    const handleShareCounter = () => {
+        dispatch(shareCounter(true))
+    }
+
+    const showShareComponent = () => (
+        <OverlayTrigger trigger="click" placement="top" overlay={shareComponent}>
+            <Btn title={shareIcon} theme="action" size="footer" onClick={handleShareCounter} />
+        </OverlayTrigger>
+    );
+
+    const handleDeleteCounter = () => {
+        if (selectedCounterStore.length !== 0) {
+            dispatch(deleteCounterService(selectedCounterStore))
+        } else { return console.log("error") }
+    }
+
     return (
         <>
             <Container>
                 <Row>
                     <Col xs={7} sm={8} md={6} lg={6} className="d-flex">
-                        <Btn title={trashIcon} theme="action" size="footer" />
-                        <Btn title={shareIcon} theme="action" size="footer" />
+                        <Btn title={trashIcon} theme="action" size="footer" onClick={handleDeleteCounter} />
+                        {showShareComponent()}
                     </Col>
 
                     <Col xs={5} sm={4} md={6} lg={6} className="align-right">
@@ -37,4 +55,10 @@ const Footer = () => {
     )
 }
 
-export default Footer;
+const mapStateToProps = (state) => {
+    return {
+        selectedCounterStore: state.selectedCounterStore
+    }
+}
+export default connect(mapStateToProps)(Footer);
+
