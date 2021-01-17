9782ce6a45d175bc89e8c473d6bb885a51f7b1d5
Stefany Guevara <efyguevara@gmail.com>
2021-01-17 11:19:33 -0300

    Function to search counters, show dinamic button 'Cancel' if exist information of the input and push filtered counters for the list

--- src/components/Searcher/Searcher.jsx
+++ src/components/Searcher/Searcher.jsx
@@ -1,24 +1,62 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { connect, useDispatch } from 'react-redux';
 import './index.css';
-import { Container, FormControl, InputGroup } from 'react-bootstrap';
+import { FormControl, InputGroup } from 'react-bootstrap';
 import glass from '../../assets/static/glass.svg';
+import { pushFilteredCounterAction } from '../../actions';
+import Btn from '../Btn/Btn';
 
-
-const Searcher = () => {
+const Searcher = ({ counterStore, onSearch }) => {
+    const dispatch = useDispatch();
+    const [search, setSearch] = useState("");
+    const [cancelButton, setCancelButton] = useState(false)
     const iconSearch = <img src={glass} alt="glass icon" />;
+    let newCounterList = [];
+
+    const pushCounters = () => {
+        return newCounterList = counterStore.filter((el) => el.title.toLowerCase().includes(search.toLowerCase()))
+    }
+
+    const setFocusValue = (value) => {
+        onSearch()
+        setCancelButton(value)
+        if (value === false) {
+            setSearch("")
+        }
+    }
+    const btnCancel = <Btn theme="action" title="Cancel" />
+
+    useEffect(() => {
+        pushCounters()
+        dispatch(pushFilteredCounterAction(newCounterList))
+    }, [dispatch, search])
+
     return (
         <>
-            <Container>
-                <div>
-                    <InputGroup>
-                        <InputGroup.Prepend>
-                            <InputGroup.Text>{iconSearch}</InputGroup.Text>
-                        </InputGroup.Prepend>
-                        <FormControl className="searcher-without-border" id="inlineFormInputGroup" placeholder="Search counters" />
-                    </InputGroup>
-                </div>
-            </Container>
+            <div className="searcher">
+                <InputGroup>
+                    <InputGroup.Prepend>
+                        <InputGroup.Text>{iconSearch}</InputGroup.Text>
+                    </InputGroup.Prepend>
+                    <FormControl
+                        className="searcher-without-border"
+                        id="inlineFormInputGroup"
+                        placeholder="Search counters"
+                        name="search"
+                        value={search}
+                        onChange={(event) => setSearch(event.target.value)}
+                        onFocus={() => setFocusValue(true)}
+                        onBlur={() => setFocusValue(false)}
+                    />
+                    {cancelButton ? btnCancel : null}
+                </InputGroup>
+            </div>
         </>
     )
 }
-export default Searcher;
+const mapStateToProps = (state) => {
+    return {
+        counterStore: state.counterStore,
+    }
+}
+export default connect(mapStateToProps)(Searcher);
