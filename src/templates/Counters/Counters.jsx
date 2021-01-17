2e545a79fe9ab7bf46cb0d3e93fa111f92080e2f
Stefany Guevara <efyguevara@gmail.com>
2021-01-17 15:43:04 -0300

    Read the connection state for render the component or return 'Connection error'

--- src/templates/Counters/Counters.jsx
+++ src/templates/Counters/Counters.jsx
@@ -1,13 +1,16 @@
-import React, { useCallback, useState } from 'react';
-import { connect } from 'react-redux';
+import React, { useCallback, useState, useEffect } from 'react';
+import { connect, useDispatch } from 'react-redux';
 import './index.css';
 import { Container } from 'react-bootstrap';
 import CounterList from '../../components/Counters/CounterList';
 import Footer from '../../components/Footer/Footer';
 import Search from '../../components/Searcher/Searcher';
 import CreateNewCounter from '../../components/Counters/CreateNewCounter';
+import { changeLoading } from '../../actions';
+import Btn from '../../components/Btn/Btn';
 
 const Counters = ({ counterStore, filteredCounters, createNewCounterModal }) => {
+    const dispatch = useDispatch();
     const [searching, setSearching] = useState(false);
     const counterList = <CounterList counters={searching ? filteredCounters : counterStore} />
     const newCounter = <CreateNewCounter />;
@@ -15,18 +18,39 @@ const Counters = ({ counterStore, filteredCounters, createNewCounterModal }) => 
     const handleSearching = useCallback(() => {
         setSearching(!searching)
     })
+    const refreshPage = () => {
+        if (navigator.onLine === true) {
+            window.location.reload()
+        }
+    }
 
+    const handleConnection = () => {
+        console.log("component", navigator.onLine)
+        if (navigator.onLine === false) {
+            dispatch(changeLoading(false))
+            return <div className="text-center">
+                <h4>Couldn’t load the counters</h4>
+                <p>The Internet connection appears to be offline.</p>
+                <Btn theme="action" title="Retry" onClick={refreshPage} />
+            </div>
+        } else {
+            return counterStore !== [] ? counterList :
+                <div className="text-center">
+                    <h4>No counters yet</h4>
+                    <p>“When I started counting my blessings, my whole life turned around.”—Willie Nelson</p>
+                </div>
+        }
+    }
+
+    useEffect(() => {
+        handleConnection()
+    }, [handleConnection])
     return (
         <>
             <Search onSearch={handleSearching} />
             <div className="counters">
                 <Container>
-                    {counterStore !== [] ? counterList :
-                        <div className="text-center">
-                            <h4>No counters yet</h4>
-                            <p>“When I started counting my blessings, my whole life turned around.”—Willie Nelson</p>
-                        </div>
-                    }
+                    {handleConnection()}
                 </Container>
             </div>
             {createNewCounterModal === true ? newCounter : null}
