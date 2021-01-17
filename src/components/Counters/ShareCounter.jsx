3283549fa19faf1d2c46909746d6df0076c8d8db
Stefany Guevara <efyguevara@gmail.com>
2021-01-17 11:28:44 -0300

    - Show information of the selected counter in the '>popover' - Function to copy text of the selected counter whith the button

--- src/components/Counters/ShareCounter.jsx
+++ src/components/Counters/ShareCounter.jsx
@@ -1,15 +1,34 @@
-import React from 'react';
-import { Popover } from 'react-bootstrap';
+import React, { useState } from 'react';
+import { Popover, Col } from 'react-bootstrap';
 import Btn from '../Btn/Btn';
+import { useDispatch } from 'react-redux';
+import { shareCounter } from '../../actions';
 
-const ShareCounter = () => {
-    return (
-            <Popover id="popover-basic">
-              <Popover.Title as="h3">Share '#' counter</Popover.Title>
-              <Popover.Content>
-                <Btn theme="action" title="Copy" />
-              </Popover.Content>
-            </Popover>
-    )
+const ShareCounter = (props) => {
+  const dispatch = useDispatch();
+
+  const [showPopover, setShowPopover] = useState(true)
+  let setAllCountersSelected = `${props.selectedCounter.map(el => el.count)} x ${props.selectedCounter.map(el => el.title)}`
+
+  const copyText = () => {
+    navigator.clipboard.writeText(setAllCountersSelected)
+    dispatch(shareCounter(false))
+  }
+
+  return (
+    <Popover id="popover-basic" className="share-counter" show={showPopover}>
+      <Col>
+        <Popover.Title as="h3">Share {props.selectedCounter.length} counter</Popover.Title>
+        <Popover.Content>
+          <Btn theme="action" title="Copy" onClick={copyText} />
+        </Popover.Content>
+      </Col>
+      <Col>
+        <Popover.Content>
+          {setAllCountersSelected}
+        </Popover.Content>
+      </Col>
+    </Popover>
+  )
 }
 export default ShareCounter;
